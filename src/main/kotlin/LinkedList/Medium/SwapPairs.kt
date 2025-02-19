57d7e5b5ae54576f0cc92ebfb47532e8c9cc42ef
Shashwath Kamath <kamathsh91@gmail.com>
2025-02-19 14:15:08 -0500

    swap pairs

--- src/main/kotlin/LinkedList/Medium/SwapPairs.kt
+++ src/main/kotlin/LinkedList/Medium/SwapPairs.kt
@@ -1,11 +1,31 @@
 fun swapPairs(head: ListNode?): ListNode? {
     val dummy: ListNode? = ListNode(0)
     dummy?.next = head
-    var prev = dummy
     var current = head
+    val k = 2
+    var prevGroupEnd = dummy
+    while (current != null) {
+        var groupStart: ListNode? = current
+        var count = 0
 
-    while (current?.next != null) {
-        v
+        while (current != null && count < k) {
+            current = current.next
+            count++
+        }
+
+        if (count == k) {
+            var subCurrent = groupStart
+            var subPrev: ListNode? = null
+            for (i in 0 until k) {
+                val next = subCurrent?.next
+                subCurrent?.next = subPrev
+                subPrev = subCurrent
+                subCurrent = next
+            }
+            prevGroupEnd?.next = subPrev
+            groupStart?.next = current
+            prevGroupEnd = groupStart
+        }
     }
 
     return dummy?.next
