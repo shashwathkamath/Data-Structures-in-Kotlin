5d36e8fca79fcb8a83512d9335d632bb66125acd
Shashwath Kamath <kamathsh91@gmail.com>
2025-03-23 09:35:28 -0400

    Stack Sequences

---
+++ src/main/kotlin/DataStructures/Stack/Easy/ValidParanthesis.kt
@@ -0,0 +1,22 @@
+package DataStructures.Stack.Easy
+
+import java.util.*
+
+fun isValid(s: String): Boolean {
+    val map = mutableMapOf<Char, Char>()
+    map[')'] = '('
+    map['}'] = '{'
+    map[']'] = '['
+    val st = Stack<Char>()
+
+    for (c in s) {
+        //()[]
+        if (st.isNotEmpty() && st.peek() == map[c]) {
+            st.pop()
+        } else {
+            st.push(c)
+        }
+    }
+
+    return st.isEmpty()
+}
---
+++ src/main/kotlin/DataStructures/Stack/Easy/ValidateStackSequences.kt
@@ -0,0 +1,5 @@
+package DataStructures.Stack.Easy
+
+fun validateStackSequences(pushed: IntArray, popped: IntArray): IntArray? {
+    return null
+}
