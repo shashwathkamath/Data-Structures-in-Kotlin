baadd969cba99e03fcc623b5d6178c0c848f7083
Shashwath Kamath <kamathsh91@gmail.com>
2025-03-24 11:25:48 -0400

    Stack Simulation

--- src/main/kotlin/DataStructures/Stack/Easy/ValidateStackSequences.kt
+++ src/main/kotlin/DataStructures/Stack/Easy/ValidateStackSequences.kt
@@ -1,5 +1,19 @@
 package DataStructures.Stack.Easy
 
-fun validateStackSequences(pushed: IntArray, popped: IntArray): IntArray? {
-    return null
+import java.util.*
+
+
+fun validateStackSequences(pushed: IntArray, popped: IntArray): Boolean {
+    val st = Stack<Int>()
+    var poppedIndex = 0
+
+    for (num in pushed) {
+        st.push(num)
+        while (st.isNotEmpty() != null && st.peek() == popped[poppedIndex]) {
+            st.pop()
+            poppedIndex++
+        }
+    }
+
+    return st.isEmpty()
 }
