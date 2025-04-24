e033d5d459db5de1e4f973c3c0bee142a61e36ff
Shashwath Kamath <kamathsh91@gmail.com>
2025-04-24 16:10:58 -0400

    new problems

---
+++ src/main/kotlin/DataStructures/Trees/lowestCommonAncestor/Solution1003.kt
@@ -0,0 +1,38 @@
+package DataStructures.Trees.lowestCommonAncestor
+
+import DataStructures.Trees.TreeNode
+
+class Solution1003 {
+
+    fun distanceBetweenNodes(root: TreeNode?, valA: Int, valB: Int): Int {
+        val lRoot = lca(root, valA, valB)
+        val distanceA = distance(valA, Pair(lRoot, 0))
+        val distanceB = distance(valB, Pair(lRoot, 0))
+        return distanceA + distanceB
+    }
+
+    private fun distance(valueToFind: Int, pa: Pair<TreeNode?, Int>): Int {
+        if (pa.first == null) return -1
+        if (valueToFind == pa.first?.`val`) {
+            return pa.second
+        }
+        val left = distance(valueToFind, Pair(pa.first?.left, pa.second + 1))
+        val right = distance(valueToFind, Pair(pa.first?.right, pa.second + 1))
+        return if (left == -1) right else left
+    }
+
+    private fun lca(root: TreeNode?, valA: Int, valB: Int): TreeNode? {
+        if (root == null) return null
+        if (root.`val` == valA || root.`val` == valB) {
+            return root
+        }
+        val left = lca(root.left, valA, valB)
+        val right = lca(root.right, valA, valB)
+
+        return when {
+            left != null && right != null -> root
+            left != null -> left
+            else -> right
+        }
+    }
+}
