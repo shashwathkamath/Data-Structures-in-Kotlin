6aac1c121288c8bf36b86495fe07bfefc0b62251
Shashwath Kamath <kamathsh91@gmail.com>
2025-05-28 04:44:20 -0400

    new problems

---
+++ src/main/kotlin/Algorithms/BinarySearch/Fundamentals/Solution540.kt
@@ -0,0 +1,23 @@
+package Algorithms.BinarySearch.Fundamentals
+
+class Solution540 {
+
+    fun singleNonDuplicate(nums: IntArray): Int {
+        var low = 0
+        var high = nums.size - 1
+
+        while (low < high) {
+            var mid = low + (high - low) / 2
+            if (mid % 2 == 1) {
+                mid = mid - 1
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2
+            } else {
+                high = mid
+            }
+        }
+
+        return nums[low]
+    }
+}
