5e3b1803e94269ff7fda9d32184978a69ba2e4d7
Shashwath Kamath <kamathsh91@gmail.com>
2025-06-01 05:37:15 -0400

    new problems

---
+++ src/main/kotlin/Prep/Main.kt
@@ -0,0 +1,14 @@
+package Prep
+
+fun main() {
+
+    Solution1().twoSum(
+        intArrayOf(2, 7, 11, 15),
+        9
+    )
+
+}
+
+fun createHelloWorld(name: String): () -> String {
+    return { "Hello $name" }
+}
