61d6fb2285b7ed399e6283115c87ce1973481af1
Shashwath Kamath <kamathsh91@gmail.com>
2025-06-16 09:04:17 -0400

    new prep problems

---
+++ src/main/kotlin/Prep/Solution92.kt
@@ -0,0 +1,34 @@
+package Prep
+
+class Solution92 {
+
+    fun reverseBetween(head: ListNode?, left: Int, right: Int): ListNode? {
+        val dummy: ListNode? = ListNode(0)
+        dummy?.next = head
+        var curr = head
+        var prev = dummy
+        var i = 1
+
+        while (curr != null && i < left) {
+            prev = curr
+            curr = curr.next
+            i++
+        }
+
+        var prevG: ListNode? = null
+        i = 0
+
+        while (curr != null && i < right - left + 1) {
+            val next = curr.next
+            curr.next = prevG
+            prevG = curr
+            curr = next
+            i++
+        }
+
+        prev?.next?.next = curr
+        prev?.next = prevG
+
+        return dummy?.next
+    }
+}
