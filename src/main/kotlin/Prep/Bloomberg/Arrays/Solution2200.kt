e44f8a5c0c89770d388b638b2e325f186b64d746
Shashwath Kamath <kamathsh91@gmail.com>
2025-07-03 08:46:21 -0400

    Arrays prep

---
+++ src/main/kotlin/Prep/Bloomberg/Arrays/Solution2200.kt
@@ -0,0 +1,28 @@
+package Prep.Bloomberg.Arrays
+
+import kotlin.math.abs
+
+class Solution2200 {
+
+    fun findKDistantIndices(nums: IntArray, key: Int, k: Int): List<Int> {
+        //find the occurence of key in nums
+        val set = mutableSetOf<Int>()
+        nums.forEachIndexed { i, num ->
+            if (num == key) {
+                set.add(i)
+            }
+        }
+        val res = mutableListOf<Int>()
+
+        for (i in 0 until nums.size) {
+            for (j in set) {
+                if (abs(i - j) <= k) {
+                    res.add(i)
+                    break
+                }
+            }
+        }
+
+        return res
+    }
+}
