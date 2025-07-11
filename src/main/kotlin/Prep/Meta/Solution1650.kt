d6a723fff43f5b9044f1d006a8d153df62a81832
Shashwath Kamath <kamathsh91@gmail.com>
2025-07-11 17:15:12 -0400

    prep

---
+++ src/main/kotlin/Prep/Meta/Solution1650.kt
@@ -0,0 +1,29 @@
+package Prep.Meta
+
+import Prep.epam.TreeNode
+
+class Solution1650 {
+
+    fun lowestCommonAncestor(p: Node?, q: Node?): Node? {
+        if (p == null || q == null) return null
+        val ancestors = mutableSetOf<Node?>()
+        var current = p
+        while (current != null) {
+            ancestors.add(current)
+            current = current.parent
+        }
+        current = q
+        while (current != null) {
+            if (ancestors.contains(current)) return current
+            current = current.parent
+        }
+
+        return null
+    }
+
+    class Node {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+        var parent: Node? = null
+    }
+}
---
+++ src/main/kotlin/Prep/Meta/util/NestedInteger.kt
@@ -0,0 +1,20 @@
+package Prep.Meta.util
+
+class NestedInteger {
+    constructor()
+    constructor(value: Int)
+
+    fun isInteger(): Boolean {
+        TODO()
+    }
+
+    fun getInteger(): Int? {
+        TODO()
+    }
+
+    fun setInteger() {}
+    fun add(ni: NestedInteger) {}
+    fun getList(): List<NestedInteger>? {
+        TODO()
+    }
+}
